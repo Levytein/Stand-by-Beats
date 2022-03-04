da1f3bc193be059adc390317d607c00083d3e8ed
Geo Ju <giho.ju@gmail.com>
2022-03-04 06:46:27 -0500

    Procedural generation pt.2

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPoint : MonoBehaviour
+public class RoomSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
