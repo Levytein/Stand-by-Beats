6d4aae98177d895bca0fd4ae0e0d68128be4df28
Levytein <giho.ju@gmail.com>
2022-09-28 01:20:17 -0400

    Martini Phase 1

---
+++ Assets/MartiniAttacks.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartiniAttacks : MonoBehaviour
+{
+
+    public int attackDamage;
+    public float secondattackDamage;
+
+    public Vector3 attackOffset;
+    public float attackRange = 2f;
+    public GameObject Slash;
+
+    public HealthController HealthManager;
+
+    public LayerMask attackMask;
+    private GameObject playerPos;
+    public Martini MartiniControl;
+    // Start is called before the first frame update
+
+   
+    public void Attack()
+    { 
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+
+        playerPos = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPosition = playerPos.transform.position;
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+      
+        if(MartiniControl.isFlipped == true)
+        {
+            Instantiate(Slash, pos, new Quaternion(0, 0, 0, 0));
+        }
+        else if(MartiniControl.isFlipped != true)
+        {
+            
+             Instantiate(Slash, pos, new Quaternion(0, 180, 0, 0));
+         
+        }
+        
+        
+        
+        if (colInfo != null)
+        {
+            Debug.Log("I hit the player");
+            HealthManager.playerHealth = HealthManager.playerHealth - attackDamage;
+            HealthManager.UpdateHealth();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
