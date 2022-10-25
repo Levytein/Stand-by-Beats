0bf01283818a2dac8d5e405e4bdab4c677da5691
Levytein <90423010+Levytein@users.noreply.github.com>
2022-10-25 19:34:39 -0400

    Pause Menu including options done + small animations

--- Assets/Scripts/SceneManagement/Menu.cs
+++ Assets/Scripts/SceneManagement/Menu.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+
 public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,20 +17,64 @@ public class Menu : MonoBehaviour
     public AudioMixer audioMixer;
 
     public GameObject PauseMenu;
+    public GameObject GraphicMenu;
+    public GameObject SoundMenu;
    
     const string MIXER_SFX = "SFXVolume";
     const string MIXER_SONG = "BGVolume";
 
     public GameObject OptionsMenu;
 
+    //Resolution UI
     public bool menuOpen = false;
 
+    public bool isSettingsopen = false;
+    public TMP_Dropdown resolutionDropDown;
+
+    Resolution[] resolutions;
 
     private void Start()
     {
         audioMixer.SetFloat("volume", 0);
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width+ "x" +  resolutions[i].height;
+            options.Add(option);
+
+
+            if(resolutions[i].width  == Screen.currentResolution .width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+
+        }
+
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
     }
 
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
     public void OptionMenu()
     {
       
@@ -38,6 +86,32 @@ public class Menu : MonoBehaviour
     {
         OptionsMenu.SetActive(false);
         PauseMenu.SetActive(true);
+    }
+    public void OpenGraphicSettings()
+    {
+        GraphicMenu.SetActive(true);
+        isSettingsopen = true;
+        if (isSettingsopen == true)
+        {
+            SoundMenu.SetActive(false);
+            
+        }
+        else
+        {
+
+        }
+    }
+    public void OpenSoundSettings()
+    {
+        SoundMenu.SetActive(true);
+        if(isSettingsopen == true)
+        {
+            GraphicMenu.SetActive(false);
+        }
+        else
+        {
+            
+        }
     }
     
     public void SetVolume(float volume)
@@ -72,16 +146,20 @@ public class Menu : MonoBehaviour
     public void BackHub()
     {
         SceneManager.LoadScene(2);
+        Time.timeScale = 1;
     }
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
 
     }
-    
+
     public void Reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+
     }
 
     IEnumerator LoadScene()
