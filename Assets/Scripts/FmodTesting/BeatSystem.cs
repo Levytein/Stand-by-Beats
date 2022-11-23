c3591636346ae47a548a149b71d4c6efa30f9420
Jonathan Dutkus <jdutkus@gmu.edu>
2022-11-22 19:57:21 -0500

    More music added and art imported

--- Assets/Scripts/FmodTesting/BeatSystem.cs
+++ Assets/Scripts/FmodTesting/BeatSystem.cs
@@ -1,27 +1,3 @@
-//--------------------------------------------------------------------
-//
-// This is a Unity behaviour script that demonstrates how to use
-// timeline markers in your game code. 
-//
-// Timeline markers can be implicit - such as beats and bars. Or they 
-// can be explicity placed by sound designers, in which case they have 
-// a sound designer specified name attached to them.
-//
-// Timeline markers can be useful for syncing game events to sound
-// events.
-//
-// The script starts a piece of music and then displays on the screen
-// the current bar and the last marker encountered.
-//
-// This document assumes familiarity with Unity scripting. See
-// https://unity3d.com/learn/tutorials/topics/scripting for resources
-// on learning Unity scripting. 
-//
-// For information on using FMOD example code in your own programs, visit
-// https://www.fmod.com/legal
-//
-//--------------------------------------------------------------------
-
 using FMOD.Studio;
 using FMODUnity;
 using System;
@@ -34,6 +10,8 @@ class BeatSystem : MonoBehaviour
     [SerializeField, Range(0, 30)] public int roomsCleard = 0;
     [SerializeField, Range(0, 3)] public int level = 1;
     [SerializeField, Range(0, 3)] public int bossPhase = 0;
+    [SerializeField, Range(0, 3)] public int paused = 0;
+    [SerializeField] private bool doDestroy = false;
 
     public static BeatSystem instance;
 
@@ -54,9 +32,6 @@ class BeatSystem : MonoBehaviour
     public delegate void BeatEventDelegate();
     public static event BeatEventDelegate beatUpdated;
 
-    public delegate void MarkerListenerDelegate();
-    public static event MarkerListenerDelegate markerUpdated;
-
     public float timeSinceLastBeat = 0;
     public float GraceTime = 0.5f;
 
@@ -66,7 +41,7 @@ class BeatSystem : MonoBehaviour
 
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        if(!doDestroy) DontDestroyOnLoad(this.gameObject);
 
         timelineInfo = new TimelineInfo();
 
@@ -100,6 +75,7 @@ class BeatSystem : MonoBehaviour
         musicInstance.setParameterByName("Rooms Cleared", roomsCleard);
         musicInstance.setParameterByName("Flags", level);
         musicInstance.setParameterByName("Boss Phase", bossPhase);
+        musicInstance.setParameterByName("Pause Status", paused);
 
         musicInstance.getTimelinePosition(out timelineInfo.currentPosition);
     }
@@ -128,7 +104,7 @@ class BeatSystem : MonoBehaviour
 
     void OnGUI()
     {
-        GUILayout.Box(String.Format("Current Bar = {0}, Last Marker = {1}", timelineInfo.currentBeat, (string)timelineInfo.lastMarker));
+        GUILayout.Box(String.Format("Current Beat = {0}", timelineInfo.currentBeat));
     }
 
     [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
@@ -155,12 +131,6 @@ class BeatSystem : MonoBehaviour
                     {
                         var parameter = (FMOD.Studio.TIMELINE_BEAT_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_BEAT_PROPERTIES));
                         timelineInfo.currentBeat = parameter.beat;
-                    }
-                    break;
-                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER:
-                    {
-                        var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
-                        timelineInfo.lastMarker = parameter.name;
                     }
                     break;
             }
